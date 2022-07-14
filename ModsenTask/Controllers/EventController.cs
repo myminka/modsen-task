6f50a84509399faceb8bccd87e64fc5aead22a26
Silkou Maxim <maxim.silkou.2002@gmail.com>
2022-07-14 22:48:09 +0300

    add EventController sceleton

--- ModsenTask/Controllers/EventController.cs
+++ ModsenTask/Controllers/EventController.cs
@@ -1,6 +1,47 @@
-﻿namespace ModsenTask.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ModsenTask.Controllers
 {
-    public class EventController
+    [ApiController]
+    [Route("events")]
+    public class EventController : ControllerBase
     {
+        public EventController()
+        {
+
+        }
+
+        [HttpGet(Name = "GetEvents")]
+        public async Task<ActionResult<IEnumerable<object>>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("{id}", Name = "GetEventById")]
+        public async Task<ActionResult<Object>> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost(Name = "CreateEvent")]
+        public async Task<IActionResult> Create(object even)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut("{id}", Name = "UpdateEvent")]
+        public async Task<IActionResult> Update(int id, object even)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete("{id}", Name = "DeleteEvent")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
