daec2f0fa49b06efc705626f66d9296a604dfdeb
Silkou Maxim <maxim.silkou.2002@gmail.com>
2022-07-15 00:49:03 +0300

    add model data annotation and EventDataContext

--- ModsenTask.Services.EntityFrameworkCore.Events/Entities/Address.cs
+++ ModsenTask.Services.EntityFrameworkCore.Events/Entities/Address.cs
@@ -1,21 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace ModsenTask.Services.EntityFrameworkCore.Events.Entities
 {
     public class Address
     {
+        [Column("country")]
         public string Country { get; set; }
 
+        [Column("city")]
         public string City { get; set; }
 
+        [Column("street")]
         public string Street { get; set; }
 
+        [Column("house_number")]
         public int HouseNumber { get; set; }
 
+        [Column("event_data_id")]
         public int EventDataId { get; set; }
 
+        [Column("event_data")]
         public EventData EventData { get; set; }
     }
 }
--- ModsenTask.Services.EntityFrameworkCore.Events/Entities/EventData.cs
+++ ModsenTask.Services.EntityFrameworkCore.Events/Entities/EventData.cs
@@ -1,19 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace ModsenTask.Services.EntityFrameworkCore.Events.Entities
 {
     public class EventData
     {
+        [Key]
+        [Column("id")]
         public int Id { get; set; }
 
+        [Column("name")]
+        [Required]
+        [StringLength(40)]
         public string Name { get; set; }
 
+        [Column("discription", TypeName = "ntext")]
         public string Description { get; set; }
 
+        [Column("organiser")]
         public string Organiser { get; set; }
 
+        [Column("address")]
         public Address Address { get; set; }
     }
 }
