332b42c4c49b9856cbc7550062e0acdfc637e0aa
Silkou Maxim <maxim.silkou.2002@gmail.com>
2022-07-15 19:14:52 +0300

    add endpoints for controllers and add controllers to services

--- ModsenTask/Startup.cs
+++ ModsenTask/Startup.cs
@@ -24,6 +24,8 @@ namespace ModsenTask
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+
             services.AddDbContext<EventDataContext>(opts =>
             {
                 opts.UseSqlServer(
@@ -45,10 +47,7 @@ namespace ModsenTask
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
 
             SeedData.EnsurePopulated(app);
