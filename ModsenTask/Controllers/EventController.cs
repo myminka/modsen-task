ae438434589b22f63e8d3e4ec8a4680d065c7914
Silkou Maxim <maxim.silkou.2002@gmail.com>
2022-07-18 13:30:56 +0300

    add xml comments for EventController

--- ModsenTask/Controllers/EventController.cs
+++ ModsenTask/Controllers/EventController.cs
@@ -17,12 +17,21 @@ namespace ModsenTask.Controllers
             _eventService = service ?? throw new ArgumentNullException(nameof(service));
         }
 
+        /// <summary>
+        /// Gets all events.
+        /// </summary>
+        /// <returns>Events.</returns>
         [HttpGet(Name = "GetEvents")]
         public async Task<ActionResult<IEnumerable<EventData>>> GetAll()
         {
             return (List<EventData>) await _eventService.ShowAllEvents();
         }
 
+        /// <summary>
+        /// Get event by Id.
+        /// </summary>
+        /// <param name="id">Id of event.</param>
+        /// <returns>Event.</returns>
         [HttpGet("{id}", Name = "GetEventById")]
         public async Task<ActionResult<EventData>> GetById(int id)
         {
@@ -36,6 +45,11 @@ namespace ModsenTask.Controllers
             return eventData;
         }
 
+        /// <summary>
+        /// Creates new event.
+        /// </summary>
+        /// <param name="eventData">Event.</param>
+        /// <returns></returns>
         [HttpPost(Name = "CreateEvent")]
         public async Task<IActionResult> Create(EventData eventData)
         {
@@ -48,6 +62,12 @@ namespace ModsenTask.Controllers
             return this.CreatedAtAction(nameof(Create), eventData);
         }
 
+        /// <summary>
+        /// Updates event.
+        /// </summary>
+        /// <param name="id">Event Id.</param>
+        /// <param name="eventData">Event.</param>
+        /// <returns></returns>
         [HttpPut("{id}", Name = "UpdateEvent")]
         public async Task<IActionResult> Update(int id, EventData eventData)
         {
@@ -64,6 +84,11 @@ namespace ModsenTask.Controllers
             return this.NotFound();
         }
 
+        /// <summary>
+        /// Delete event.
+        /// </summary>
+        /// <param name="id">Event Id.</param>
+        /// <returns></returns>
         [HttpDelete("{id}", Name = "DeleteEvent")]
         public async Task<IActionResult> Delete(int id)
         {
